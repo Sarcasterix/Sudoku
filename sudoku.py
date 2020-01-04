efa9c2f6def36a7660a57416d85f387519fc3ee9
Alx <atmichaelson@gmail.com>
2020-01-04 14:46:28 +0000

    Fixing naming conventions in sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -32,18 +32,18 @@ class Board:
     """
     Basic getter functions
     """
-    def getNum(self, x, y):
-        return self.grid[y][x]
+    def getNum(self, col, row):
+        return self.grid[row][col]
 
-    def getOrig(self, x, y):
-        return self.originals[y][x]
+    def getOrig(self, col, row):
+        return self.originals[row][col]
 
     """
     Basic Setter function
     """
-    def setNum(self, x, y, n):
-        if self.isValid(x, y, n):
-            self.grid[y][x] = n
+    def setNum(self, col, row, n):
+        if self.isValid(col, row, n):
+            self.grid[row][col] = n
         else:
             print("Invalid Move")
 
@@ -58,46 +58,46 @@ class Board:
     """
     Basic Get functions for row and column. X and Y are reversed for the data structure used here.
     """
-    def getRow(self, y):
-        return self.grid[y]
+    def getRow(self, row):
+        return self.grid[row]
 
-    def getCol(self, x):
+    def getCol(self, col):
         toReturn = []
-        for y in range(9):
-            toReturn.append(self.grid[y][x])
+        for row in range(9):
+            toReturn.append(self.grid[row][col])
         return toReturn    
 
     """
     Returns the 3x3 subsquare, alongside the top-left co-ordinates of that square
     """
-    def getSqr(self, x, y):
+    def getSqr(self, col, row):
         positionals = [[0, 1, 2], [3, 4, 5], [6, 7, 8]]
         for pos in positionals:
-            if x in pos:
+            if col in pos:
                 cols = pos
-            if y in pos:
+            if row in pos:
                 rows = pos
         toReturn = []
-        tlxy = [(x, y) for x in cols for y in rows]
-        for x in cols:
-            for y in rows:
-                toReturn.append(self.grid[y][x])
-        return toReturn, tlxy
+        sqrCoords = [(col, row) for col in cols for row in rows]
+        for col in cols:
+            for row in rows:
+                toReturn.append(self.grid[row][col])
+        return toReturn, sqrCoords
     
-    def getPossibilities(self, x, y):
-        sqrPoss = theSet - set(self.getSqr(x,y)[0])
-        rowPoss = theSet - set(self.getRow(y))
-        colPoss = theSet - set(self.getCol(x))
+    def getPossibilities(self, col, row):
+        sqrPoss = theSet - set(self.getSqr(col,row)[0])
+        rowPoss = theSet - set(self.getRow(row))
+        colPoss = theSet - set(self.getCol(col))
         poss_vals = list(sqrPoss.intersection(rowPoss).intersection(colPoss))
         #poss_vals = list((theSet - set(self.getSqr(x,y)[0])).intersection(theSet - set(self.getRow(y))).intersection(theSet - set(self.getCol(x))))
         return poss_vals
 
     """
-    Tests for Validity of setting grid(x, y) to n.
+    Tests for Validity of setting grid(col, row) to n.
     Using set subtraction, we whittle down from 1-9 to only numbers not present in row, column and s
     """
-    def isValid(self, x, y, n):
-        possibles = self.getPossibilities(x, y)
+    def isValid(self, col, row, n):
+        possibles = self.getPossibilities(col, row)
         if n in possibles:
             return True
         return False
@@ -106,13 +106,13 @@ class Board:
     Solves all squares with only one possible number.
     '''
     def findSingle(self):
-        for y in range(9):
-            for x in range(9):
-                if (self.grid[y][x]) == 0 and len(self.getPossibilities(x, y)) == 1:
-                    if x == 4 and y == 1:
+        for row in range(9):
+            for col in range(9):
+                if (self.grid[row][col]) == 0 and len(self.getPossibilities(col, row)) == 1:
+                    if col == 4 and row == 1:
                         pass
-                    print("The only possible number at position {}, {} is {}".format(x, y, self.getPossibilities(x, y)[0]))
-                    self.setNum(x, y, self.getPossibilities(x, y)[0])
+                    print("The only possible number at position {}, {} is {}".format(col, row, self.getPossibilities(col, row)[0]))
+                    self.setNum(col, row, self.getPossibilities(col, row)[0])
                     return True
         return False
 
@@ -133,9 +133,9 @@ class Board:
     '''
     def findSqr(self, coords):
         toReturn = []
-        for x, y in coords:
-            if self.grid[y][x] == 0:
-                toReturn.extend(self.getPossibilities(x, y))
+        for col, row in coords:
+            if self.grid[row][col] == 0:
+                toReturn.extend(self.getPossibilities(col, row))
         return set(toReturn)
 
     '''
@@ -143,20 +143,20 @@ class Board:
     around our input tile to check if this tile has a possibility no other has. In that case, that must be
     what we input.
     '''
-    def findMulti(self, x, y):
-        if self.grid[y][x] == 0:
-            tilePossSet = set(self.getPossibilities(x, y))
+    def findMulti(self, col, row):
+        if self.grid[row][col] == 0:
+            tilePossSet = set(self.getPossibilities(col, row))
 
-            rowPoss = self.findColRow(x, self.getRow(y))
-            colPoss = self.findColRow(y, self.getCol(x))
-            sqrCoords = self.getSqr(x, y)[1]
-            sqrCoords.remove((x, y))
+            rowPoss = self.findColRow(col, self.getRow(row))
+            colPoss = self.findColRow(row, self.getCol(col))
+            sqrCoords = self.getSqr(col, row)[1]
+            sqrCoords.remove((col, row))
             sqrPoss = self.findSqr(sqrCoords)
             tilePoss = tilePossSet - rowPoss - colPoss - sqrPoss
             if len(tilePoss) == 1:
                 value = tilePoss.pop()
-                print("By induction, the only value possible at {}, {} is {}".format(x, y, value))
-                self.setNum(x, y, value)
+                print("By induction, the only value possible at {}, {} is {}".format(col, row, value))
+                self.setNum(col, row, value)
                 return True         
         else:
             print("The given tile is already filled")
